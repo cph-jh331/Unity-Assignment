e868b85baed01bb3846e7536bd89d6c2b605a182
John Hansen <cph-jh331@cphbusiness.dk>
2019-08-27 11:06:43 +0200

    ok

---
+++ Assignment2/Assignment 2/Assets/A2scripts/HitDetectorHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetectorHandler : MonoBehaviour
+{
+
+    public GameObject explositionFx;
+    public GameObject Enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            explositionFx = Instantiate(explositionFx, transform.position, transform.rotation);
+            print("bummelum");
+            Destroy(Enemy.gameObject);
+            Destroy(explositionFx.gameObject, 2);
+        }
+    }
+}
